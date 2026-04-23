cc7302c958b7a3430c642e23cd323198f8f0f5b8
XDmzjing <675100672@qq.com>
2026-04-23 10:40:07 +0800

    基本

--- src/main/java/org/example/mapper/StudentMapper.java
+++ src/main/java/org/example/mapper/StudentMapper.java
@@ -1,6 +1,8 @@
 package org.example.mapper;
 
+
 import org.apache.ibatis.annotations.Insert;
+
 
 public interface StudentMapper {
     @Insert("insert into db_student (sid,name,age) values (3,'小明',18)")
